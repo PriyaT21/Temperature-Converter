a11b042165755706a2d6b3d925bf570241da8874
Priyanka <priya.m.tripathi@gmail.com>
2022-04-28 19:39:57 +0200

    js update

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ var fah = document.getElementById("fah");
 cel.addEventListener('input', function(){
     let C = this.value;
     let f = (C * 9/5) + 32;
+   if(!Number.isInteger(f)){
+       f = f.toFixed(4);
+   }
     fah.value = f;
 });
 
 fah.addEventListener('input', function(){
     let f = this.value;
     let c = (f - 32) * 5/9;
+    if(!Number.isInteger(c)){
+        c = c.toFixed(4);
+    }
+    
     cel.value = c;
 });

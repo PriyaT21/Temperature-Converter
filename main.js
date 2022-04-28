57b57fd228d8e61e98b13c1c2ce0fd5a3328931a
Priyanka <priya.m.tripathi@gmail.com>
2022-04-28 14:42:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var cel = document.getElementById("cel");
+var fah = document.getElementById("fah");
+
+cel.addEventListener('input', function(){
+    let C = this.value;
+    let f = (C * 9/5) + 32;
+    fah.value = f;
+});
+
+fah.addEventListener('input', function(){
+    let f = this.value;
+    let c = (f - 32) * 5/9;
+    cel.value = c;
+});
